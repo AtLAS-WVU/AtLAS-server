cbf38f090d7cccef6d440932c9003572481d9704
Oliver Wiegand <owiegand@gmail.com>
2018-03-03 16:48:26 -0500

    Added code to deny a request.

---
+++ DenyRequest.php
@@ -0,0 +1,107 @@
+<?php
+require ('databasevaribles.php');
+
+$username = $_POST['username'];
+$token = $_POST['token'];
+$requestID = $_POST['requestID'];
+
+if (empty($username)) {
+    $response['success'] = FALSE;
+    $response['debug'] = "A username must be entered!";
+    echo json_encode($response);
+    die();
+}
+
+if (empty($token)) {
+    $response['success'] = FALSE;
+    $response['debug'] = "An token must be entered!";
+    echo json_encode($response);
+    die();
+}
+
+if (empty($requestID)) {
+    $response['success'] = FALSE;
+    $response['debug'] = "A request ID must be entered!";
+    echo json_encode($response);
+    die();
+}
+
+// Connect To MYSQL
+$conn = mysqli_connect($dbhostname, $dbusername, $dbpassword);
+
+// If we can't connect to the database exit.
+if (!isset($conn)) {
+    $response['success'] = FALSE;
+    $reponse['debug'] = "Unable to connect to MYSQL";
+    echo json_encode($response);
+    die();
+}
+
+// Select Correct Database
+if (!mysqli_select_db($conn,$dbname)) {
+    $response['success'] = FALSE;
+    $response['debug'] = "Unable to connect to database";
+    echo json_encode($response);
+    die();
+}
+
+// Escape the input passed in by the user
+$username = mysqli_real_escape_string($conn, $username);
+$token = mysqli_real_escape_string($conn, $token);
+$requestID = mysqli_real_escape_string($conn, $requestID);
+
+// Confirm that the token requested is correct
+$result = mysqli_query($conn, "SELECT userid FROM users WHERE username = '$username' AND private_key = '$token'");
+
+if (!$result) {
+    // MYSQL Error
+    $response['success'] = FALSE;
+    $response['debug'] = "MYSQL query failed: ".mysqli_error($conn);
+    echo json_encode($response);
+    die();
+} else if (mysqli_num_rows($result) == 0) {
+    // User's token doesn't match
+    $response['success'] = FALSE;
+    $response['debug'] = "User token doesn't match";
+    echo json_encode($response);
+    die();
+}
+
+// Get the user's ID
+$row = mysqli_fetch_assoc($result);
+$userID = $row['userid'];
+
+
+// Check to make sure that the user can approve the request
+$FriendCheckQuery = "SELECT * FROM requests WHERE requestID = '$requestID' AND revceiverID = '$userID'";
+$result = mysqli_query($conn, $FriendCheckQuery);
+if (!$result) {
+    // MYSQL Error
+    $response['success'] = FALSE;
+    $response['debug'] = "MYSQL query failed: ".mysqli_error($conn);
+    echo json_encode($response);
+    die();
+} else if (mysqli_num_rows($result) == 0) {
+    // User's token doesn't match
+    $response['success'] = FALSE;
+    $response['debug'] = "You have no requests to approve with the given request ID";
+    echo json_encode($response);
+    die();
+}
+
+// Delete the request
+$DeleteQuery = "DELETE FROM requests where requestID='$requestID'";
+$result = mysqli_query($conn, $DeleteQuery);
+if (!$result) {
+    // MYSQL Error
+    $response['success'] = FALSE;
+    $response['debug'] = "MYSQL query failed: ".mysqli_error($conn);
+    echo json_encode($response);
+    die();
+}
+
+$response['success'] = TRUE;
+echo json_encode($response);
+die();
+
+?>
